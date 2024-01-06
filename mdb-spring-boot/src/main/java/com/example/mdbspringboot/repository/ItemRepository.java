5f808335412a37cc0ba19fb48a6cbba78ba3b585
ismaamah <maher.ismaail@diconium.com>
2024-01-06 14:27:51 +0100

    create MongoDB-Mongock-Spring simple project

--- mdb-spring-boot/src/main/java/com/example/mdbspringboot/repository/ItemRepository.java
+++ mdb-spring-boot/src/main/java/com/example/mdbspringboot/repository/ItemRepository.java
@@ -2,11 +2,14 @@ package com.example.mdbspringboot.repository;
 
 import java.util.List;
 
+import com.example.mdbspringboot.MdbSpringBootApplication;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import com.example.mdbspringboot.model.GroceryItem;
+import org.springframework.stereotype.Repository;
 
+@Repository(MdbSpringBootApplication.CLIENTS_COLLECTION_NAME)
 public interface ItemRepository extends MongoRepository<GroceryItem, String> {
 	
 	@Query("{name:'?0'}")
